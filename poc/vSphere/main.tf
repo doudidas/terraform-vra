b3a199f80d3784436b339770e8065151855eee59
Edouard Topin <ed.topin@gmail.com>
2022-02-25 15:59:15 +0100

    ready2

--- poc/vSphere/main.tf
+++ poc/vSphere/main.tf
@@ -9,9 +9,23 @@ terraform {
 
 provider "vsphere" {
   vsphere_server       = "10.225.3.223"
-  user                 = "administrator@vsphere.local"
-  password             = "VMware1!"
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
   allow_unverified_ssl = true
+}
+
+variable "vsphere_user" {
+  description = "User to connect on vsphere"
+  default = "administrator@vsphere.local"
+  sensitive   = true
+}
+variable "vm_name" {
+  description = "name of the VM"
+}
+variable "vsphere_password" {
+  description = "password to connect on vsphere"
+  default = "VMware1!"
+  sensitive   = true
 }
 
 data "vsphere_datacenter" "dc" {
@@ -26,7 +40,7 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 data "vsphere_compute_cluster" "compute_cluster" {
-  name          = "Cluster"
+  name          = "Admin"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 data "vsphere_content_library" "this" {
@@ -35,7 +49,7 @@ data "vsphere_content_library" "this" {
 data "vsphere_content_library_item" "CentOS8x64" {
   library_id = data.vsphere_content_library.this.id
   name       = "CentOS8x64"
-  type = "ovf"
+  type       = "ovf"
 
 }
 
@@ -55,13 +69,27 @@ resource "vsphere_virtual_machine" "vm" {
 
   resource_pool_id = resource.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
-
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
   disk {
-    label = "disk0"
-    size  = 20
+    label       = "disk1"
+    size        = 20
+    unit_number = 0
+  }
+  disk {
+    label       = "disk0"
+    size        = 20
+    unit_number = 1
   }
   depends_on = [
     data.vsphere_content_library_item.CentOS8x64
   ]
 }
 
+output "vm" {
+  value = resource.vsphere_virtual_machine.vm
+}
+output "disks" {
+  value = resource.vsphere_virtual_machine.vm.disk
+}
